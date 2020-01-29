d87ac20399dad3724c3e08c6768a91e2de46d759
Joe <jodeph.broughton@gmail.com>
2020-01-29 16:30:07 +0000

    Added the picking up item Raycast, just need to modify it to work as we need it to

---
+++ GAM130Prototype/Assets/Scripts/PickUpMechanic.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMechanic : MonoBehaviour
+{
+    public bool canPick;
+    public GameObject uiPick;
+    //public Transform target;
+    public float speed;
+    //public GameObject Player;
+
+    public Rigidbody[] childRigidbody;
+    private GameObject lastObject;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        canPick = false;
+        uiPick.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3f))
+        {
+            Debug.DrawRay(transform.position, transform.forward * 3f, Color.red);
+
+            if (hit.collider.GetComponent<PickedObject>() != null)
+            {
+                canPick = true;
+                uiPick.SetActive(true);
+                Debug.Log("Hit");
+            }
+
+            else
+            {
+                uiPick.SetActive(false);
+                canPick = false;
+            }
+
+            if (Input.GetMouseButtonDown(0) && canPick == true)
+            {
+                hit.collider.GetComponent<PickedObject>().IsPickedUp = true;
+                //Destroy(hit);
+               
+
+
+            }
+
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            if (lastObject != null)
+            {
+                lastObject.GetComponent<PickedObject>().IsPickedUp = false;
+            }
+
+            childRigidbody = GetComponentsInChildren<Rigidbody>();
+            foreach (Rigidbody body in childRigidbody)
+            {
+                body.useGravity = true;
+                body.isKinematic = false;
+            }
+            transform.DetachChildren();
+        }
+    }
+}
---
+++ GAM130Prototype/Assets/Scripts/PickedObject.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Food
+{
+    FoodValue = 0,
+    WaterValue = 1
+
+};
+
+
+public class PickedObject : MonoBehaviour
+{
+    public Food Name;
+
+    public float yOffset;
+    private Rigidbody RB;
+
+    private Coroutine coroutine;
+
+    private bool isPickedup;
+    public bool IsPickedUp
+    {
+        get { return isPickedup; }
+
+        set
+        {
+            if (coroutine != null) StopCoroutine(coroutine);
+            if (!value) coroutine = StartCoroutine(StartDelay());
+            else if (value) isPickedup = value;
+        }
+    }
+   
+    private void Awake()
+    {
+        RB = this.GetComponent<Rigidbody>();
+    }
+
+
+    private IEnumerator StartDelay()
+    {
+        //print(RB.velocity.magnitude);
+
+        yield return new WaitForSeconds(.5f);
+
+        while (RB.velocity.magnitude > 0.25f)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        isPickedup = false;
+    }
+}
