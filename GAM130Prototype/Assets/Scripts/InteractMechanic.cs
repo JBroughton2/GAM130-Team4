ce4e29b3e23d76b8017c91f47d7402b245c5986c
Joe <jodeph.broughton@gmail.com>
2020-01-29 17:46:05 +0000

    Cleaned up some of the project and my scripts. But I seem to have broken something in my InteractMechanic so I will try again soon

--- GAM130Prototype/Assets/Scripts/InteractMechanic.cs
+++ GAM130Prototype/Assets/Scripts/InteractMechanic.cs
@@ -1,32 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InteractMechanic : MonoBehaviour
 {
-    public GameObject interactUI;
+    public bool canPick;
+    public GameObject uiPick;
+    public GameObject uiInteract;
 
-    // Start is called before the first frame update
+    // Use this for initialization
     void Start()
     {
-        
+        canPick = false;
+        uiPick.SetActive(false);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit, 3f))
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 3f))
         {
             Debug.DrawRay(transform.position, transform.forward * 3f, Color.red);
-            if (hit.collider != null)
+
+            if (hit.collider.GetComponent<PickedObject>() != null)
             {
-                interactUI.SetActive(true);
+                canPick = true;
+                uiPick.SetActive(true);
+                Debug.Log("PickUp");
             }
+
+            if (hit.collider.GetComponent<InteractableButton>() != null)
+            {
+                hit.collider.GetComponent<InteractableButton>().buttonClicked = true;
+                uiInteract.SetActive(true);
+                Debug.Log("Interactable");
+            }
+
             else
             {
-                interactUI.SetActive(false);
+                uiPick.SetActive(false);
+                canPick = false;
             }
+
+            if (Input.GetMouseButtonDown(0) && canPick == true)
+            {
+                hit.collider.GetComponent<PickedObject>().IsPickedUp = true;
+                Destroy(hit.transform.gameObject);
+                //Add something to the resources value when we have them added. I plan to use enums for different objects
+
+
+            }
+
         }
+
     }
 }
---
+++ GAM130Prototype/Assets/Scripts/InteractableButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableButton : MonoBehaviour
+{
+    public bool buttonClicked;
+    void Start()
+    {
+        buttonClicked = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
