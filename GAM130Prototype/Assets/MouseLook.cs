1e19c0e3c6c4db2bfc7e969bf45a75e0fdf7be90
Joe <jodeph.broughton@gmail.com>
2020-01-31 19:17:47 +0000

    Created nice new fps controller. Works with slopes and stairs now too, and has easy to edit values :)

--- GAM130Prototype/Assets/MouseLook.cs
+++ GAM130Prototype/Assets/MouseLook.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class MouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 100f;
-
+    public Transform playerBody;
+    private float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -18,5 +19,11 @@ public class MouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation += mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
     }
 }
---
+++ GAM130Prototype/Assets/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement Variables")]
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundMask;
+    public float jumpHeight;
+
+    private bool isGrounded;
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //creates a small sphere that outputs a check value depending on what is in sphere
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        
+        //resets the velocity value if the player is grounded and not falling anymore.
+        if(isGrounded && velocity.y < 0)
+        {
+            //is negative two just in case it happens before player hits the floor
+            velocity.y = -2;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //this is the vector that uses the values of x and z to register player input
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+        
+        //this builds the velocity so the player can fall as if gravity is acting on it
+        velocity.y += gravity * Time.deltaTime;
+        //this is using that velocity value to actually move the player down
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
