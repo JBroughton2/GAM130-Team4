6ec81fe3f73805a00e9b33d19933cc5816947ed9
gj241211 <gj241211@falmouth.ac.uk>
2020-02-01 15:37:22 +0000

    Added HUD TestScene.unity and HudAssets. Made an Oxygen bar that depreciates over time.

---
+++ GAM130Prototype/Assets/HudAssets/OxygenBarScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenBarScript : MonoBehaviour
+{
+    bool decreasing = true;
+    float oxygen = (float)1.0;
+    float Ammount_Decrease = (float)0.0005;
+   
+    private void Nothing() {
+
+    }
+
+    
+    private void Update()
+    {
+        
+        if ((oxygen > Ammount_Decrease) && (decreasing = true))
+        {
+            oxygen = oxygen - Ammount_Decrease;
+
+            Transform bar = transform.Find("Bar");
+            bar.localScale = new Vector3(oxygen, 1f);
+
+            Invoke("Nothing", 0.1f);
+        }
+
+
+
+
+    }
+}
