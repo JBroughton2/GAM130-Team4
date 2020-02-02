68cfe0a6c24dabfd5acd9f6a5f6ff75dd14c6c37
Joe <jodeph.broughton@gmail.com>
2020-02-02 15:53:27 +0000

    Added a sprint function to the player controller

--- GAM130Prototype/Assets/PlayerMovement.cs
+++ GAM130Prototype/Assets/PlayerMovement.cs
@@ -8,6 +8,9 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float x;
+    public float z;
+    public float sprintMultiplier;
     public Transform groundCheck;
     public float groundDistance;
     public LayerMask groundMask;
@@ -31,34 +34,52 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = -2;
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
-
-        //this is the vector that uses the values of x and z to register player input
-        Vector3 move = transform.right * x + transform.forward * z;
-
-        controller.Move(move * speed * Time.deltaTime);
-        
-        //Jump mechanic using a formula for calulating the force needed to jump relative to the gravity in the game
-        if(Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
-        }
-
-        if (Input.GetKeyDown(KeyCode.F) && !flashlightOn)
-        {
-            flashlight.SetActive(true);
-            flashlightOn = true;
-        }else if (Input.GetKeyDown(KeyCode.F) && flashlightOn)
-        {
-            flashlight.SetActive(false);
-            flashlightOn = false;
-        }
-
+        PlayerMove();
+        PlayerJump();
+        Flashlight();
         
         //this builds the velocity so the player can fall as if gravity is acting on it
         velocity.y += gravity * Time.deltaTime;
         //this is using that velocity value to actually move the player down
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void PlayerMove()
+    {
+
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+        //this is the vector that uses the values of x and z to register player input
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Vector3 sprint = transform.right * x + transform.forward * z;
+            controller.Move(sprint * speed * sprintMultiplier * Time.deltaTime);
+        }
+    }
+    void PlayerJump()
+    {
+        //Jump mechanic using a formula for calulating the force needed to jump relative to the gravity in the game
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
+    }
+
+    void Flashlight()
+    {
+
+        if (Input.GetKeyDown(KeyCode.F) && !flashlightOn)
+        {
+            flashlight.SetActive(true);
+            flashlightOn = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && flashlightOn)
+        {
+            flashlight.SetActive(false);
+            flashlightOn = false;
+        }
     }
 }
