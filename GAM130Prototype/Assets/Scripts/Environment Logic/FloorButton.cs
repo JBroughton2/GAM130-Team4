9f0daaa2cfc4f6a9249dd927ffd1e24e783ad2b3
Robomoo <robomooer@gmail.com>
2020-02-05 19:48:46 +0000

    Environment prefabs.
    -Implemented all planned button prototypes
    -Made environment prefabs to showcase

---
+++ GAM130Prototype/Assets/Scripts/Environment Logic/FloorButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class FloorButton : MonoBehaviour
+{
+    public UnityEvent m_action;
+    public UnityEvent m_reverseAction;
+
+    public Animator m_buttonAnimator = null;
+
+    public string[] m_triggerTags;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    bool checkTag(string tag)
+    {
+        bool foundTag = tag == "Player";
+        for (int i = 0; i < m_triggerTags.Length && !foundTag; i++)
+        {
+            foundTag = tag == m_triggerTags[i];
+        }
+
+        return foundTag;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (checkTag(other.gameObject.tag))
+        {
+            m_action.Invoke();
+
+            if (m_buttonAnimator != null) m_buttonAnimator.SetBool("Pressed", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (checkTag(other.gameObject.tag))
+        {
+            m_reverseAction.Invoke();
+
+            if (m_buttonAnimator != null) m_buttonAnimator.SetBool("Pressed", false);
+        }
+    }
+
+}
