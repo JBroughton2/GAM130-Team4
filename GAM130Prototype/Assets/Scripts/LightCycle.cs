172ba438347adb4199242960bd89e72157104995
mcgeecaitlin <55736882+mcgeecaitlin@users.noreply.github.com>
2020-02-18 17:30:20 +0000

    Updated LightCycle.cs

--- GAM130Prototype/Assets/Scripts/LightCycle.cs
+++ GAM130Prototype/Assets/Scripts/LightCycle.cs
@@ -10,14 +10,24 @@ public class LightCycle : MonoBehaviour
     public float currentTime = 0;
     public float timeMultiplier = 1f;
 
+    float sunInitialIntensity;
+    float moonInitialIntensity;
 
     void Start()
     {
+        GameObject sun = this.gameObject;
+        GameObject moon = this.gameObject;
 
+        
+
+        sunInitialIntensity = sun.intensity;
+        moonInitialIntensity = moon.intensity;
     }
-
     void Update()
     {
+        UpdateSun();
+        UpdateMoon();
+
         currentTime += (Time.deltaTime / secondsInDay) * timeMultiplier;
         if (currentTime >= 1)
         {
@@ -26,5 +36,43 @@ public class LightCycle : MonoBehaviour
 
         transform.RotateAround(Vector3.zero, Vector3.right, (60f / secondsInDay) * Time.deltaTime);
         transform.LookAt(Vector3.zero);
+    }
+
+    void UpdateSun()
+    {
+        float intensityMultiplier = 1;
+        if (currentTime <= 0.23f || currentTime >= 0.75f)
+        {
+            intensityMultiplier = 0;
+        }
+        else if (currentTime <= 0.25f)
+        {
+            intensityMultiplier = Mathf.Clamp01((currentTime - 0.23f) * (1 / 0.02f));
+        }
+        else if (currentTime >= 0.73f)
+        {
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTime - 0.73f) * (1 / 0.02f)));
+        }
+
+        sun.intensity = sunInitialIntensity * intensityMultiplier;
+    }
+
+    void UpdateMoon()
+    {
+        float intensityMultiplier = 1;
+        if (currentTime <= 0.23f || currentTime >= 0.75f)
+        {
+            intensityMultiplier = 0;
+        }
+        else if (currentTime <= 0.25f)
+        {
+            intensityMultiplier = Mathf.Clamp01((currentTime - 0.23f) * (1 / 0.02f));
+        }
+        else if (currentTime >= 0.73f)
+        {
+            intensityMultiplier = Mathf.Clamp01(1 - ((currentTime - 0.73f) * (1 / 0.02f)));
+        }
+
+        moon.intensity = moonInitialIntensity * intensityMultiplier;
     }
 }
