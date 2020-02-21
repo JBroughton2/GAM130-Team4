924cdac84d73be8c6994c96a6dc0b0f626aa73ff
Joe Broughton <jodeph.broughton@gmail.com>
2020-02-21 11:49:38 +0000

    Water death box started, needs some polish

---
+++ GAM130Prototype/Assets/WaterDeath.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterDeath : MonoBehaviour
+{
+    [SerializeField]
+    private Transform checkpoint;
+    [SerializeField]
+    private GameObject fadeToBlack;
+    private float delay = 0.5f;
+
+    void OnTriggerEnter(Collider col)
+    {
+        fadeToBlack.SetActive(true);
+        col.transform.position = checkpoint.position;
+        StartCoroutine(fadeDelay());
+    }
+
+    IEnumerator fadeDelay()
+    {
+        yield return new WaitForSeconds(delay);
+        fadeToBlack.SetActive(false);
+    }
+
+}
