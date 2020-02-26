c05f0dc10a4d0e3e0eea47cc5a376757de5b40de
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-02-26 18:00:29 +0000

    Made the spider move
    * Created new animation controller

--- GAM130Prototype/Assets/Scripts/SpiderController.cs
+++ GAM130Prototype/Assets/Scripts/SpiderController.cs
@@ -3,23 +3,47 @@ using UnityEngine.AI;
 
 public class SpiderController : MonoBehaviour
 {
-    [SerializeField]
-    private Transform target;
-    [SerializeField]
-    private Animator anim;
-
+    [SerializeField] private Transform target;
+    [SerializeField] private Animator anim;
     private NavMeshAgent navMesh;
 
-
+    private bool inPLay = false;
+    private bool attacking;
+ 
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
     }
+
 
     void Update()
     {
-        Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
-        navMesh.destination = newTargetPostition;
 
+        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("DigEmerge")) inPLay = true;
+
+        if (inPLay)
+        {
+            Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            navMesh.destination = newTargetPostition;
+            anim.SetBool("WalkForwards", true);
+            if (attacking)
+            {
+                anim.SetBool("Attack", false);
+                attacking = false;
+            }
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Triggered");
+            anim.SetBool("Attack", true);
+            attacking = true;
+        }
     }
 }
