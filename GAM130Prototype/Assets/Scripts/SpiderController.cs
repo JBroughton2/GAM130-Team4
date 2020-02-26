eb6dc89be6a9282bd7235b041c09a8e98cc33641
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-02-26 14:08:41 +0000

    Adjusted SpiderController.cs and added material to the spider in SpiderTest scene

--- GAM130Prototype/Assets/Scripts/SpiderController.cs
+++ GAM130Prototype/Assets/Scripts/SpiderController.cs
@@ -5,33 +5,30 @@ public class SpiderController : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
+
     [SerializeField]
     private Animator anim;
-
     private NavMeshAgent navMesh;
     Vector3 lastFacing = Vector3.zero;
-
 
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
         lastFacing = transform.forward;
     }
 
     void Update()
     {
-
         navMesh.destination = target.position;
-
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             anim.SetBool("Moving", true);
         }
-
         Vector3 currentFacing = transform.forward;
         float currentAngularVelocity = Vector3.Angle(currentFacing, lastFacing) / Time.deltaTime; //degrees per second
         lastFacing = currentFacing;
-
-        bool isRotating = currentAngularVelocity > 1f;
     }
+
+
 }
