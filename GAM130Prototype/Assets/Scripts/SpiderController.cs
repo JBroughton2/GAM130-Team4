3b903e2476281e903ad75b3e38a9ba761929e0f3
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-02-26 16:02:41 +0000

    Updated SpiderController.cs
    Updated SpiderTest.cs - this script contains code that I will be using but is currently redundant.
    Made changes to the scene SpiderTest - added extra walls to test spider navigation.
    Corrected nav mesh

--- GAM130Prototype/Assets/Scripts/SpiderController.cs
+++ GAM130Prototype/Assets/Scripts/SpiderController.cs
@@ -5,30 +5,21 @@ public class SpiderController : MonoBehaviour
 {
     [SerializeField]
     private Transform target;
-
     [SerializeField]
     private Animator anim;
+
     private NavMeshAgent navMesh;
-    Vector3 lastFacing = Vector3.zero;
+
 
     void Start()
     {
         navMesh = GetComponent<NavMeshAgent>();
-        anim = GetComponent<Animator>();
-        lastFacing = transform.forward;
     }
 
     void Update()
     {
-        navMesh.destination = target.position;
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            anim.SetBool("Moving", true);
-        }
-        Vector3 currentFacing = transform.forward;
-        float currentAngularVelocity = Vector3.Angle(currentFacing, lastFacing) / Time.deltaTime; //degrees per second
-        lastFacing = currentFacing;
+        Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+        navMesh.destination = newTargetPostition;
+
     }
-
-
 }
