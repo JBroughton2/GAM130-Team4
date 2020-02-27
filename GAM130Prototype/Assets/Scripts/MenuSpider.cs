3dfce8735ff916368a7b410ab2bca87674d7fadf
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-02-27 17:08:38 +0000

    Moving spider across the menu
    *Created 2 waypoints for spider to move between.
    *Deleted little spider from scene

--- GAM130Prototype/Assets/Scripts/MenuSpider.cs
+++ GAM130Prototype/Assets/Scripts/MenuSpider.cs
@@ -1,18 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 public class MenuSpider : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent navMesh;
+
     void Start()
     {
-        
+        navMesh = GetComponent<NavMeshAgent>();
+        navMesh.autoBraking = false;
+        GotoNextPoint();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!navMesh.pathPending && navMesh.remainingDistance < 0.5f)
+            GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        destPoint = (destPoint + 1) % points.Length;
     }
 }
