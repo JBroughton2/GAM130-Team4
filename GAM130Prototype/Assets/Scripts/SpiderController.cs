eed920248df4edd36ae722704932eeeb25877afc
RobinFalkner-Wedge <58291625+RobinFalkner-Wedge@users.noreply.github.com>
2020-03-02 12:26:34 +0000

    once again tried to fix the player prefab and stuff,
    fixed the player prefab, i should probably avoid doing anything else with it for a while.

---
+++ GAM130Prototype/Assets/Scripts/SpiderController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderController : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private Animator anim;
+    private NavMeshAgent navMesh;
+
+    private bool inPLay = false;
+    private bool attacking;
+ 
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+
+    void Update()
+    {
+
+        if(!anim.GetCurrentAnimatorStateInfo(0).IsName("DigEmerge")) inPLay = true;
+
+        if (inPLay)
+        {
+            Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
+            navMesh.destination = newTargetPostition;
+            anim.SetBool("WalkForwards", true);
+            if (attacking)
+            {
+                anim.SetBool("Attack", false);
+                attacking = false;
+            }
+
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Triggered");
+            anim.SetBool("Attack", true);
+            attacking = true;
+        }
+    }
+}
