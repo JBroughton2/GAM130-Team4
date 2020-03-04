c03414fd6942c46cbf810b4d2b8dfcdb8b269d91
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-03-04 14:03:15 +0000

    Patrolling Spider
    * Made changes to SpiderPatrol.cs
    * Spider will now patrol between three way points
    * Attempted to make spider wait for 5 seconds at each waypoint

--- GAM130Prototype/Assets/Scripts/SpiderOrientation.cs
+++ GAM130Prototype/Assets/Scripts/SpiderOrientation.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 public class SpiderOrientation : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    [SerializeField] private Animator anim;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+
+        agent.autoBraking = false;
+        GotoNextPoint();
     }
 
-    // Update is called once per frame
+    void GotoNextPoint()
+    {
+        if (points.Length == 0)
+            return;
+        agent.destination = points[destPoint].position;
+
+        destPoint = (destPoint + 1) % points.Length;
+        anim.SetBool("WalkForwards", true);
+
+        StartCoroutine(PointDelay());
+    }
+
     void Update()
     {
-        
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            StartCoroutine(PointDelay());
+        }
+
+    }
+
+    IEnumerator PointDelay()
+    {
+        if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            yield return new WaitForSeconds(10);
+        }
+        Debug.Log("Pause");
+        GotoNextPoint();
     }
 }
