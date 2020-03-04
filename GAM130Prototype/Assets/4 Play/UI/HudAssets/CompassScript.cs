648afa07522eecdb8c150b92d1aeb3c9c60afcfd
GJ241211 <55745197+GJ241211@users.noreply.github.com>
2020-03-04 17:58:03 +0000

    cleaned up had prefab
    Added a daynight clock - currently set with test values.

---
+++ GAM130Prototype/Assets/4 Play/UI/HudAssets/CompassScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassScript : MonoBehaviour
+{
+    public int dayLength = 360;
+    public float rotateAmount;
+    public float interval;
+    Vector3 dir;
+    
+    private void Update()
+    {
+        interval = - dayLength / 360 * Time.deltaTime;
+
+
+        rotateAmount = rotateAmount + interval;
+        dir.z = rotateAmount;
+
+        transform.localEulerAngles = dir;
+    }
+
+
+    
+
+}
