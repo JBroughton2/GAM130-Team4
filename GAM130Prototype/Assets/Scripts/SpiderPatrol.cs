d47d1d5cf9e0c3db8f198ac8d8c0ecca5b198144
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-03-06 13:05:31 +0000

    Updated SpiderPatrol.cs
    Spider will now attack player when in a specific prximty.

--- GAM130Prototype/Assets/Scripts/SpiderPatrol.cs
+++ GAM130Prototype/Assets/Scripts/SpiderPatrol.cs
@@ -4,30 +4,38 @@ using System.Collections;
 
 public class SpiderPatrol : MonoBehaviour
 {
+    // Waypoints
     public Transform[] points;
     private int destPoint = 0;
 
+    // Referencing things
     [SerializeField] private Animator anim;
     private NavMeshAgent agent;
     private Transform target;
+    // private PlayerHealth playerHealth;
 
-    private PlayerHealth playerHealth;
+    // Radius for chasing and attacking.
+    public float chaseRadius = 25f;
+    public float attackRadius = 10f;
+
+    // Attacking values
     public float timeBetweenAttacks = 0.5f;
     public int attackDamage = 10;
-    public float attackRadius;
 
-
+    public bool isNearbyPlayer;
+    public bool readyToAttack;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
-        playerHealth = playerHealth.GetComponent<PlayerHealth>();
+        // playerHealth = playerHealth.GetComponent<PlayerHealth>();
 
         agent.autoBraking = false;
         GotoNextPoint();
     }
 
+    // Function which makes patrolling spiders travel between waypoints.
     void GotoNextPoint()
     {
         if (points.Length == 0)
@@ -40,53 +48,87 @@ public class SpiderPatrol : MonoBehaviour
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        bool isNearbyPlayer = IsPlayerNear();
-        if (!agent.pathPending && agent.remainingDistance < 0.5f && !isNearbyPlayer)
+        isNearbyPlayer = IsPlayerNear();
+        readyToAttack = playerInRange();
+
+        if (!agent.pathPending && agent.remainingDistance < 0.5f && !isNearbyPlayer && !readyToAttack)
         {
             StartCoroutine(PointDelay());
         }
-        if (isNearbyPlayer)
+        if (isNearbyPlayer && !readyToAttack)
         {
+            Debug.Log("Chasing");
             Chase();
         }
+        if (readyToAttack)
+        {
+            Debug.Log("Attacking");
+            Attack();
+        }
     }
+
+    
 
     IEnumerator PointDelay()
     {
         anim.SetBool("WalkForwards", false);
         yield return new WaitForSeconds(7);
         GotoNextPoint();
-        //Debug.Log("Delaying");
     }
 
+    // Function to make spider chase plaer when close.
     void Chase()
     {
         Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
         agent.destination = newTargetPostition;
-        //Debug.Log("Is Chasing");
     }
 
+    // Function for spider to attack player when close.
+    void Attack()
+    {
+        anim.SetBool("Attack", true);
+    }
+
+    // Seeing if the player is close enough to the spider.
     bool IsPlayerNear()
+    {
+        Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, chaseRadius);
+        foreach (Collider item in nearbyObjects)
+        {
+            if (item.CompareTag("Player"))
+            {
+                target = item.transform;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // Seeing if player is close enough to spider to be attacked.
+    bool playerInRange()
     {
         Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, attackRadius);
         foreach (Collider item in nearbyObjects)
         {
-            Debug.Log(item);
-
             if (item.CompareTag("Player"))
             {
                 target = item.transform;
-                Debug.Log("Player near is true.");
                 return true;
             }
         }
+
         return false;
     }
 
+    // Creates a red radius around character.
     private void OnDrawGizmos()
     {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(this.transform.position, chaseRadius);
+
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(this.transform.position, attackRadius);
     }
