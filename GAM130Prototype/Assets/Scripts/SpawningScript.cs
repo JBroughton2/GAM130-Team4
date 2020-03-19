41312aa55c4ae84ebbe762c1ffbb701211a15f1f
RobinFalkner-Wedge <robinfalknerwedge@gmail.com>
2020-03-19 18:36:49 +0000

    minor changes to tunnels
    added some more parts to the ditch and altered the spawner script.

--- GAM130Prototype/Assets/Scripts/SpawningScript.cs
+++ GAM130Prototype/Assets/Scripts/SpawningScript.cs
@@ -8,7 +8,7 @@ public class SpawningScript : MonoBehaviour
     public GameObject Player;
     public Vector3 SpawnPos;
     public GameObject SpawnItem;
-
+    public bool itemSpawned;
 
 
     // Start is called before the first frame update
@@ -20,9 +20,10 @@ public class SpawningScript : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == Player)
+        if (other.gameObject == Player && itemSpawned == true)
         {
             Instantiate(SpawnItem, SpawnPos, Quaternion.identity);
+            itemSpawned = false;
         }
     }
 }
