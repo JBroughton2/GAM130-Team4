ce452b406a48bb0bd1aa5f2231fbd11026d92920
RobinFalkner-Wedge <robinfalknerwedge@gmail.com>
2020-03-21 18:12:47 +0000

    made more edits to tunnel level
    added particle effects when the spider spawns, tried to fix the scale of the decorative river part (still isn't small enough) and added fire and water particle effects to the cave.
    At some point the fire must dissappear and the water must appear when the power is turned back on.

--- GAM130Prototype/Assets/Scripts/SpawningScript.cs
+++ GAM130Prototype/Assets/Scripts/SpawningScript.cs
@@ -9,7 +9,8 @@ public class SpawningScript : MonoBehaviour
     public Vector3 SpawnPos;
     public GameObject SpawnItem;
     public bool itemSpawned;
-
+    public GameObject particleSFX;
+    public Vector3 SpawnPos2;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,7 @@ public class SpawningScript : MonoBehaviour
         if (other.gameObject == Player && itemSpawned == true)
         {
             Instantiate(SpawnItem, SpawnPos, Quaternion.identity);
+            Instantiate(particleSFX, SpawnPos2, Quaternion.identity);
             itemSpawned = false;
         }
     }
